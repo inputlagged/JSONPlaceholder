f09e13d70ef309b8c040cf59fe3f3d1ce118fa5a
inputlagged <pavelseidhe@gmail.com>
2022-05-26 09:36:52 +0600

    added post cell

---
+++ JSONPlaceholder/JSONPlaceholder/View/PostTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PostTableViewCell.swift
+//  JSONPlaceholder
+//
+//  Created by Pavel Epaneshnikov on 26/5/22.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+}
--- JSONPlaceholder/JSONPlaceholder/ViewControllers/ViewController.swift
+++ JSONPlaceholder/JSONPlaceholder/ViewControllers/ViewController.swift
@@ -10,11 +10,30 @@ import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var postTableView: UITableView! {
+        didSet {
+            let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+            postTableView.register(nib, forCellReuseIdentifier: "PostTableViewCellID")
+            postTableView.delegate = self
+            postTableView.dataSource = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
         }
     
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCellID", for: indexPath) as! PostTableViewCell
+        return cell
+    }
 }
 
