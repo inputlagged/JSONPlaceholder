716a3cc99e83d6a2a4fa0b71e226a6d2df1a31de
inputlagged <pavelseidhe@gmail.com>
2022-05-26 08:33:40 +0600

    Added project

---
+++ JSONPlaceholder/JSONPlaceholder/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONPlaceholder
+//
+//  Created by Pavel Epaneshnikov on 26/5/22.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+        }
+    
+}
+
